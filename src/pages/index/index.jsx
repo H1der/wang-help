6abb434d0b964fa63ccb247239a64b48f0527cc0
Hider <724598894@qq.com>
2022-01-10 10:03:11 +0800

    Initial commit

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,35 @@
+import {View} from "@tarojs/components";
+import {AtGrid, AtCard} from 'taro-ui'
+import './index.scss'
+
+function Index() {
+  const list = [{
+    image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png',
+    value: '防疫出行政策'
+  },
+    {
+      image: 'https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png',
+      value: '垃圾分类'
+    },
+    {
+      image: 'https://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png',
+      value: '油价查询'
+    },
+    {
+      image: 'https://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png',
+      value: '快递查询'
+    },
+  ]
+
+
+  return (
+    <View className='index'>
+      <AtCard>
+        <AtGrid data={list} className='test' />
+      </AtCard>
+
+    </View>
+  );
+}
+
+export default Index;
