1a9778f30bf3033d35c0a92716c27be145d19a51
WangXin <hider99449@gmail.com>
2025-08-07 09:15:54 +0800

    调整亲戚计算器页面ui

--- src/pages/relationship/index.jsx
+++ src/pages/relationship/index.jsx
@@ -42,66 +42,84 @@ function Relationship() {
     }
   }
 
-  const keys = [{call: '爸爸', sex: 1,},
-    {call: '妈妈', sex: 0,},
-    {call: 'DEL', sex: 2,},
-    {call: 'CLR', sex: 2,},
-    {call: '老公', sex: 1,},
-    {call: '老婆', sex: 0,},
-    {call: '儿子', sex: 1,},
-    {call: '女儿', sex: 0,},
-    {call: '哥哥', sex: 1,},
-    {call: '弟弟', sex: 1,},
-    {call: '姐姐', sex: 0,},
-    {call: '妹妹', sex: 0,},]
+  const keys = [
+    {call: '爸爸', sex: 1, type: 'default'},
+    {call: '妈妈', sex: 0, type: 'default'},
+    {call: 'DEL', sex: 2, type: 'secondary'},
+    {call: 'CLR', sex: 2, type: 'secondary'},
+    {call: '老公', sex: 1, type: 'default'},
+    {call: '老婆', sex: 0, type: 'default'},
+    {call: '儿子', sex: 1, type: 'default'},
+    {call: '女儿', sex: 0, type: 'default'},
+    {call: '哥哥', sex: 1, type: 'default'},
+    {call: '弟弟', sex: 1, type: 'default'},
+    {call: '姐姐', sex: 0, type: 'default'},
+    {call: '妹妹', sex: 0, type: 'default'},
+  ]
 
 
   return (
     <View className='container'>
-      <View className='condition'>
-        <RadioGroup className='condition-group'>
-          <Label className='radio-list__label'>
-            我的性别：
-            <Radio className='radio-list__radio' color='#eb4035' value='1' onClick={() => setMySex(1)}
-              checked={mySex === 1}
-            >男姓</Radio>
-            {/*<Radio className='radio-list__radio' color='#FADC9C' value='1' onClick={() => setSelectSex(1)} checked={mySex === 1}>男姓</Radio>*/}
-          </Label>
-          <Label className='radio-list__label'>
-            <Radio className='radio-list__radio' color='#eb4035' value='0' onClick={() => setMySex(0)}
-              checked={mySex === 0}
-            >女姓</Radio>
-          </Label>
-        </RadioGroup>
-        <RadioGroup className='condition-group'>
-          <Label className='radio-list__label'>
-            称呼方式：
-            <Radio className='radio-list__radio' color='#eb4035' value='true' onClick={() => setMode(false)}
-              checked={mode === false}
-            >我称呼对方</Radio>
-          </Label>
-          <Label className='radio-list__label'>
-            <Radio className='radio-list__radio' color='#eb4035' value='false' onClick={() => setMode(true)}
-              checked={mode === true}
-            >对方称呼我</Radio>
-          </Label>
-        </RadioGroup>
+      <View className='header'>
+        <h1>亲戚关系计算器</h1>
+        <p>快速计算亲戚间的称呼关系</p>
       </View>
-      <view className='text-chain'>{relationship({text: relation.join(''), sex: mySex, reverse: mode}).join(',')}</view>
-      <view className='text-relation'>{relation}</view>
+
+      <View className='card'>
+        <View className='condition'>
+          <View className='card-title'>基本信息</View>
+          <RadioGroup className='condition-group'>
+            <Label className='radio-list__label'>
+              <Radio className='radio-list__radio' color='#4facfe' value='1' onClick={() => setMySex(1)}
+                checked={mySex === 1}
+              >男</Radio>
+            </Label>
+            <Label className='radio-list__label'>
+              <Radio className='radio-list__radio' color='#fa709a' value='0' onClick={() => setMySex(0)}
+                checked={mySex === 0}
+              >女</Radio>
+            </Label>
+          </RadioGroup>
+          <RadioGroup className='condition-group'>
+            <Label className='radio-list__label'>
+              <Radio className='radio-list__radio' color='#4facfe' value='true' onClick={() => setMode(false)}
+                checked={mode === false}
+              >我称呼对方</Radio>
+            </Label>
+            <Label className='radio-list__label'>
+              <Radio className='radio-list__radio' color='#fa709a' value='false' onClick={() => setMode(true)}
+                checked={mode === true}
+              >对方称呼我</Radio>
+            </Label>
+          </RadioGroup>
+        </View>
+      </View>
+
+      <View className='card result-container'>
+        <View className='card-title'>计算结果</View>
+        <view className='text-chain'>{relationship({text: relation.join(''), sex: mySex, reverse: mode}).join('、')}</view>
+        <view className='text-relation'>{relation.join('')}</view>
+      </View>
+
       <View className='keyboard'>
-        <Row>
-          {keys.map(key => {
-            return <Col span='6' key={key.call + key.sex}><View  className='keyboard-key'>
-              {/*如果当前选中者的性别为女性,则老婆按钮为禁用状态.反之如果是男性则老公按钮为禁用*/}
-              <Button  block className='keyboard-btn'
-                disabled={(key.call === '老婆' && selectSex[selectSex.length - 1] === 0) || (key.call === '老公' && selectSex[selectSex.length - 1] === 1)}
-                onClick={() => handleKeyboardClick(key)}
-              >{key.call}</Button>
-            </View>
-            </Col>
-          })}
-        </Row>
+        <View className='keyboard-title'>选择关系</View>
+        <View className='keyboard-row'>
+          <Row>
+            {keys.map(key => {
+              return <Col span='6' key={key.call + key.sex}>
+                <View className='keyboard-key'>
+                  {/*如果当前选中者的性别为女性,则老婆按钮为禁用状态.反之如果是男性则老公按钮为禁用*/}
+                  <Button
+                    block
+                    className={`keyboard-btn ${key.type}`}
+                    disabled={(key.call === '老婆' && selectSex[selectSex.length - 1] === 0) || (key.call === '老公' && selectSex[selectSex.length - 1] === 1)}
+                    onClick={() => handleKeyboardClick(key)}
+                  >{key.call}</Button>
+                </View>
+              </Col>
+            })}
+          </Row>
+        </View>
       </View>
     </View>
   );
