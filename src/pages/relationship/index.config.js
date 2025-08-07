a73e5913782da072010893a498566e14bab0ce6f
WangXin <hider99449@gmail.com>
2025-08-07 09:16:19 +0800

    调整亲戚计算器页面ui

--- src/pages/relationship/index.config.js
+++ src/pages/relationship/index.config.js
@@ -1,6 +1,6 @@
 export default {
   navigationBarTitleText: '亲戚计算器',
   // navigationBarBackgroundColor: '#FADC9C',
-  navigationBarBackgroundColor: '#eb4035',
-  navigationBarTextStyle: 'white',
+  // navigationBarBackgroundColor: '#eb4035',
+  // navigationBarTextStyle: 'white',
 }
